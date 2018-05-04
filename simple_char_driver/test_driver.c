3b9c319e62ab3e4841c7db5af831496ba82b8cc4
Richard Poulson <rick.poulson@colorado.edu>
2018-05-04 12:12:14 -0600

    Made progress on test_driver

--- simple_char_driver/test_driver.c
+++ simple_char_driver/test_driver.c
@@ -7,14 +7,26 @@
 #include <errno.h>
 #include <string.h>
 
+char PromptInput(void);
+int ValidChoice(char choice);
+void ReadFromDevice();
+void WriteToDevice();
+void SeekIntoDevice();
+void CallRelatedFunction(char choice);
+
 int main() {
 	char * buffer = (char *) malloc(32);
-	char input = 'c';
-	//while (input != 'e') {
-	//	input = getchar();
-	//}
 	char * data = "abcd1234\n";
 	FILE * device_file = fopen("/dev/simple_character_device","a+");
+
+	char choice;
+	while (1) {
+		choice = PromptInput();
+		while (ValidChoice(choice) == 0) { choice = PromptInput(); }
+		CallRelatedFunction(choice);
+	}
+
+
 	fputs(data, device_file);
 	fseek(device_file, 0, SEEK_SET);
 	fgets(buffer, 9, device_file);
@@ -23,4 +35,52 @@ int main() {
 	free(buffer);
 
     return (0);
+}
+
+char PromptInput(void) {
+	char input;
+	printf("\n");
+	printf("Press r to read from device\n");
+	printf("Press w to write to the device\n");
+	printf("Press s to seek into the device\n");
+	printf("Press e to exit from the device\n");
+	printf("anything else to keep reading or writing from the device\n");
+	printf("Enter command: ");
+	input = getchar();
+	printf("\n");
+	return input;
+}
+
+int ValidChoice(char choice) {
+	if (choice == 'r') return 1;
+	if (choice == 'w') return 1;
+	if (choice == 's') return 1;
+	if (choice == 'e') return 1;
+	return 0;
+}
+
+void ReadFromDevice() {
+	printf("How many bytes would you like to read from the file? ");
+	int num = getint();
+	buffer = (char *) realloc(buffer, num);
+	printf("\n");
+}
+void WriteToDevice() {
+	printf("Enter data you want to write to the device: ");
+	char a_word[50];
+	scanf ("%s", a_word);
+	printf("\n");
+    printf ("You entered: %s\n", a_word);
+}
+void SeekIntoDevice() {
+
+}
+void CallRelatedFunction(char choice) {
+	if (choice == 'r') { ReadFromDevice(); }
+	if (choice == 'w') { WriteToDevice(); }
+	if (choice == 's') { SeekIntoDevice(); }
+	if (choice == 'e') {
+		free(buffer);
+		exit(0);
+	}
 }
